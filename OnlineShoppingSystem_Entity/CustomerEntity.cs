f657b77a14697ca7f90394fb3eaa7c640eee15d8
PRADEEP <PRADEEP@LAPTOP-6FQVFC4N>
2020-02-20 19:50:16 -0800

    Validations

--- OnlineShoppingSystem_Entity/CustomerEntity.cs
+++ OnlineShoppingSystem_Entity/CustomerEntity.cs
@@ -18,7 +18,7 @@ namespace OnlineShoppingSystem_Entity
         //    Electronics
         //}
         [Required(ErrorMessage = "Enter the name")]
-        [RegularExpression(@"(\S\D)+", ErrorMessage = " Space and numbers not allowed")]
+        [RegularExpression(@"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", ErrorMessage = " Space and numbers not allowed")]
         [StringLength(30, MinimumLength = 5)]
         public string customerName
         {
@@ -26,18 +26,21 @@ namespace OnlineShoppingSystem_Entity
             set;
         }
         [Required(ErrorMessage = "Enter the Mobile Number")]
+        [RegularExpression(@"^[6789]\d{9}$", ErrorMessage ="Mobile Number is Invalid")]
         public long customerMobile
         {
             get;
             set;
         }
         [Required(ErrorMessage = "Enter your password")]
+        [RegularExpression(@"^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$", ErrorMessage = "Password must contains upper case, lower case, Numbers, and special character")]
         public string customerPassword
         {
             get;
             set;
         }
         [Required(ErrorMessage = "Enter your E-mail address")]
+        [EmailAddress]
         public string customerEMail
         {
             get;
